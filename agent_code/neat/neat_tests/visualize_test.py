f579559e37e8d5f5098c6823f3fcdca84af8be1d
Goshagosha <svezhegor@tutanota.com>
2022-03-11 18:26:44 +0100

    added visualization, fixed some bugs but in a separate file

---
+++ agent_code/neat/neat_tests/visualize_test.py
@@ -0,0 +1,19 @@
+from neat import *
+import pygame
+
+pygame.init()
+display = pygame.display.set_mode([500, 500])
+
+g = Genome.fresh()
+g.mutate(20)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    display.fill((0, 0, 0))
+    g.visualize(display)
+    pygame.display.flip()
+
+pygame.quit()
